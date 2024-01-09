d454661bccbf4c35f8020c37789f3eb664dc9127
nicolasgsantana <nicolas07san@gmail.com>
2024-01-09 16:44:06 -0300

    Add game logic

--- js/script.js
+++ js/script.js
@@ -1,10 +1,84 @@
+function createPlayer(name, token) {
+    return { name, token }
+}
+
 const gameBoard = (() => {
-    const board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
-    const placeSymbol = (position, symbol) => {
-        board[position[0]][position[1]] = symbol;
+    const board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+
+    const placeToken = (x, y, token) => {
+        if (board[y][x] === 0) {
+            board[y][x] = token;
+        }
+        else {
+            console.log(`Invalid position`);
+        }
     };
-    const print = () => {
-        console.log(board);
+
+    const getBoard = () => {
+        return board;
     };
-    return { placeSymbol, print };
+
+    return { placeToken, getBoard };
 })();
+
+const gameController = (() => {
+    const players = [createPlayer("Player 1", 1), createPlayer("Player 2", 2)];
+
+    const activePlayer = players[0];
+
+    const switchActivePlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => {
+        return activePlayer;
+    };
+
+    const victoryAchieved = () => {
+        const board = gameBoard.getBoard();
+
+        if (
+            (board[0][0] === activePlayer.token
+                && board[1][1] === activePlayer.token
+                && board[2][2] === activePlayer.token) ||
+            (board[0][2] === activePlayer.token
+                && board[1][1] === activePlayer.token
+                && board[2][0] === activePlayer.token)
+        ) {
+            return true;
+        }
+
+        for (let i = 0; i < board.length; i++) {
+            if (board[i].every((value => value === activePlayer.token))) {
+                return true;
+            }
+            else if (board[0][i] === activePlayer.token && board[1][i] === activePlayer.token
+                && board[2][i] === activePlayer.token) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    const playRound = () => {
+        console.log(`${activePlayer.name} turn`);
+        const x = Number(prompt("Enter X position: "));
+        const y = Number(prompt("Enter Y position: "));
+
+        gameBoard.placeToken(x, y, activePlayer.token);
+
+        if (victoryAchieved) {
+            console.log(`${activePlayer.name} won!`);
+        }
+        else {
+            gameBoard.getBoard();
+            switchActivePlayer();
+            playRound();
+        }
+    };
+
+    return { getActivePlayer, playRound };
+})();
+
+gameController.playRound();
