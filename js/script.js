5bc345cc67d689b260d725a08bc3f8c73a285611
nicolasgsantana <nicolas07san@gmail.com>
2024-01-31 16:30:16 -0300

    First attempt

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ function createPlayer(name, token) {
 }
 
 const gameBoard = (() => {
-    const board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    const board = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
 
-    const placeToken = (x, y, token) => {
-        if (board[y][x] === 0) {
-            board[y][x] = token;
+    const placeToken = (index, token) => {
+        if (board[index] < 0) {
+            board[index] = token;
+            return true;
         }
-        else {
-            console.log(`Invalid position`);
-        }
+
+        return false;
     };
 
     const getBoard = () => {
@@ -21,68 +21,10 @@ const gameBoard = (() => {
     return { placeToken, getBoard };
 })();
 
-const gameController = (() => {
-    const players = [createPlayer("Player 1", 1), createPlayer("Player 2", 2)];
-
-    let activePlayer = players[0];
-
-    const switchActivePlayer = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    };
-
-    const getActivePlayer = () => {
-        return activePlayer;
-    };
-
-    const victoryAchieved = () => {
-        const board = gameBoard.getBoard();
-
-        if (
-            (board[0][0] === activePlayer.token
-                && board[1][1] === activePlayer.token
-                && board[2][2] === activePlayer.token) ||
-            (board[0][2] === activePlayer.token
-                && board[1][1] === activePlayer.token
-                && board[2][0] === activePlayer.token)
-        ) {
-            return true;
-        }
-
-        for (let i = 0; i < board.length; i++) {
-            if (board[i].every((value => value === activePlayer.token))) {
-                return true;
-            }
-            else if (board[0][i] === activePlayer.token && board[1][i] === activePlayer.token
-                && board[2][i] === activePlayer.token) {
-                return true;
-            }
-        }
-
-        return false;
-    };
-
-    const playRound = () => {
-        console.log(`${activePlayer.name} turn`);
-        const x = Number(prompt("Enter X position: "));
-        const y = Number(prompt("Enter Y position: "));
-
-        gameBoard.placeToken(x, y, activePlayer.token);
-
-        if (victoryAchieved()) {
-            console.log(`${activePlayer.name} won!`);
-        }
-
-        console.log(gameBoard.getBoard());
-
-        switchActivePlayer();
-    };
-
-    return { getActivePlayer, playRound };
-})();
-
 const visualsController = (() => {
     const board = document.querySelector(".board");
     const cells = Array.from(board.children);
+    const paragraphs = document.querySelectorAll("main p")
 
     const root = document.querySelector(":root");
     const THEMES = [["#070707", "#F5F0F6"], ["#F5F0F6", "#070707"], ["#101820", "#FEE715"], ["#070952", "#ef4da0"], ["#FBEAEB", "#2E3C7E"]];
@@ -96,8 +38,108 @@ const visualsController = (() => {
         });
     }
 
+    const updateTexts = (text) => {
+        paragraphs.forEach(p => {
+            p.innerText = text;
+        });
+    }
+
     const switchColorScheme = (themeIndex) => {
         root.style.setProperty('--bg-color', THEMES[themeIndex][0]);
         root.style.setProperty('--main-color', THEMES[themeIndex][1]);
     };
+
+    const showToken = (index, playerToken) => {
+        const cell = cells[index];
+        const svg = cell.children[playerToken];
+        cell.classList.toggle("empty");
+        svg.style.display = "block";
+        console.log("Place token");
+    }
+
+    const initializeCells = () => {
+        cells.forEach(cell => {
+            cell.addEventListener('click', () => {
+                if (cell.classList.contains("empty"))
+                    gameController.playRound(cells.indexOf(cell));
+            });
+        });
+    };
+
+    return { showToken, updateTexts, updatePreviewSymbol, initializeCells }
 })();
+
+const gameController = (() => {
+    const players = [createPlayer("Player 1", 1), createPlayer("Player 2", 0)];
+
+    let activePlayer = players[0];
+
+    //Initialization
+    visualsController.updatePreviewSymbol(activePlayer.token);
+    visualsController.updateTexts(`${activePlayer.name} turn`)
+
+    const switchActivePlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const victoryAchieved = () => {
+        const board = gameBoard.getBoard();
+
+        // Sorry for this abomination
+        if (
+            // Check diagonals
+            (board[0] === activePlayer.token
+                && board[4] === activePlayer.token
+                && board[8] === activePlayer.token) ||
+            (board[2] === activePlayer.token
+                && board[4] === activePlayer.token
+                && board[6] === activePlayer.token) ||
+
+            // Check horizontals
+            (board[0] === activePlayer.token
+                && board[1] === activePlayer.token
+                && board[2] === activePlayer.token) ||
+            (board[3] === activePlayer.token
+                && board[4] === activePlayer.token
+                && board[5] === activePlayer.token) ||
+            (board[6] === activePlayer.token
+                && board[7] === activePlayer.token
+                && board[8] === activePlayer.token) ||
+
+            // Check verticals
+            (board[0] === activePlayer.token
+                && board[3] === activePlayer.token
+                && board[6] === activePlayer.token) ||
+            (board[1] === activePlayer.token
+                && board[4] === activePlayer.token
+                && board[7] === activePlayer.token) ||
+            (board[2] === activePlayer.token
+                && board[5] === activePlayer.token
+                && board[8] === activePlayer.token)
+        ) {
+            return true;
+        }
+
+        return false;
+    };
+
+    const playRound = (index) => {
+        visualsController.updateTexts(`${activePlayer.name} turn`)
+
+        if (gameBoard.placeToken(index, activePlayer.token)) {
+            visualsController.showToken(index, activePlayer.token);
+        }
+
+        if (victoryAchieved()) {
+            visualsController.updateTexts(`${activePlayer.name} won!`);
+        }
+        else {
+            switchActivePlayer();
+            visualsController.updatePreviewSymbol(activePlayer.token);
+        }
+    };
+
+    return { playRound };
+})();
+
+visualsController.initializeCells();
